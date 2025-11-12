05a5c3dd8223492cedb95e000a70a54cb03ad33a
taraskin5201 <tdumailo@gmail.com>
2025-11-12 18:54:09 +0100

    add lesson2

--- lesson2.py
+++ lesson2.py
@@ -1,2 +1,22 @@
-import numpy as np
+# # зображення у opencv
+import cv2
 
+# читання злображення
+img = cv2.imread(
+    'data/lesson1/cameraman.png',  # шлях до файлу
+    cv2.IMREAD_GRAYSCALE # формат зображення
+)
+
+
+print(type(img))
+print(img)
+print(img.shape)
+print(img.dtype)
+
+# показати зображення
+cv2.imshow(
+    'image',   # назву
+    img
+)
+
+cv2.waitKey(0)

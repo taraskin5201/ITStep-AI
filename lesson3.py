12f337b55caf3cdc17afafe5e98a6ac7d02b4c08
taraskin5201 <tdumailo@gmail.com>
2025-11-17 20:18:19 +0100

    add lesson3

--- lesson3.py
+++ lesson3.py
@@ -69,38 +69,76 @@
 #
 # utils.lesson2_hsv_range()
 
+# import cv2
+#
+#
+# img = cv2.imread('data/lesson2/lego.jpg', cv2.IMREAD_COLOR)
+# img = cv2.resize(img, (500, 500))
+#
+# # img -- bgr
+# cv2.imshow('bgr', img)
+#
+# # перевести в hsv
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#
+# print(hsv.shape)
+# print(hsv.dtype)
+#
+# # # думає що hsv -- bgr
+# # cv2.imshow('hsv', hsv)
+#
+# # h -- 100-130
+# # s -- 150-255
+# # v -- 140-255
+# mask_blue = cv2.inRange(
+#     hsv,
+#     (100, 150, 140), # нижні межі
+#     (130, 255, 255)  # верхні межі
+# )
+#
+# print(mask_blue.shape)
+# print(mask_blue.dtype)
+#
+# cv2.imshow('mask_blue', mask_blue)
+#
+# cv2.waitKey(0)
+
+
+
+# Відкрийте зображення data/lesson2/cell.png. Покращте
+# зображення за допомогою вирівнювання гістограми. Оскільки
+# зображення кольорове, вам доведеться зробити наступні
+# кроки:
+#  перевести зображення в LAB
+#  розбити зображення на канали l, a та b
+#  вирівняти гістограму для l
+#  зібрати канали назад в зображення
+#  перевести результат назад в BGR
+# Порівняйте результати для 2 алгоритмів.
+
+
 import cv2
 
-
-img = cv2.imread('data/lesson2/lego.jpg', cv2.IMREAD_COLOR)
-img = cv2.resize(img, (500, 500))
-
-# img -- bgr
-cv2.imshow('bgr', img)
-
-# перевести в hsv
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-print(hsv.shape)
-print(hsv.dtype)
-
-# # думає що hsv -- bgr
-# cv2.imshow('hsv', hsv)
-
-# h -- 100-130
-# s -- 150-255
-# v -- 140-255
-mask_blue = cv2.inRange(
-    hsv,
-    (100, 150, 140), # нижні межі
-    (130, 255, 255)  # верхні межі
-)
-
-print(mask_blue.shape)
-print(mask_blue.dtype)
-
-cv2.imshow('mask_blue', mask_blue)
-
-cv2.waitKey(0)
-
-
+# img = cv2.imread('data/lesson2/cell.png', cv2.IMREAD_COLOR)
+# img = cv2.resize(img, (500, 500))
+#
+# lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+#
+# l = lab[:, :, 0]
+# a = lab[:, :, 1]
+# b = lab[:, :, 2]
+#
+#
+# new_l = cv2.equalizeHist(l)
+#
+# lab[:, :, 0] = new_l
+#
+# cv2.imshow('new_l', new_l)
+#
+# new_img = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+#
+# cv2.imshow('new_img', new_img)
+#
+# cv2.imshow('original', img)
+#
+# cv2.waitKey(0)

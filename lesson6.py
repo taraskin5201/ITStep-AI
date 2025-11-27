8c23a526c53eab5091917fdb97092cdb126a15f2
taraskin5201 <tdumailo@gmail.com>
2025-11-27 19:37:57 +0100

    add lesson6

--- lesson6.py
+++ lesson6.py
@@ -1,96 +1,97 @@
 # детекція об'єктів(YOLO)
 
-import ultralytics
-import cv2
-
-# створення моделі
-model = ultralytics.YOLO('yolov8s.pt')
-
-# отримати зображення з ввідео
-cap = cv2.VideoCapture('data/lesson8/cars+bikes.mp4')
-success, img = cap.read()
-
-img = cv2.resize(img, None, fx=0.5, fy=0.5)
-
-cv2.imshow('original', img)
-
-# застосування моделі
-# predict може отримувати декілька зображень за раз
-# predict([img1, img2, img3]) і тому results це список
-# [result1, result2, result3]
-
-results = model.predict(
-    img,    # саме зображення
-    device='cpu',    # процесор де запускати модель
-                     # cpu -- звичайний процес
-                     # cuda -- графічний процесор
-    conf=0.25,       # мінімальній відсоток для об'єкта щоб попасти в result
-    iou=0.7      # максимально можливий рівень перетину рамок(якщо більше то вважаємо
-                 # що це рамки для одного й того самого об'єкта)
-)
-
-
-# results -- список з одним елементом
-result = results[0]
-
-# отримати назви класів(об'єкти які вмієме визначати модель)
-names = result.names # dict --- id: str
-print(names)
-
-# самі об'єкти
-cls = result.boxes.cls
-print(cls)
-
-# візуалізація результів
-res_img = result.plot()
-cv2.imshow('result', res_img)
-
-
-# ймовірності
-conf = result.boxes.conf
-print(conf)
-
-
-# рамка(box)
-boxes = result.boxes
-
-# отримати перший об'єкт
-box = boxes[0]
-
-print(box)
-
-# координати
-xyxy = box.xyxy[0]
-
-# переведення координат в int
-x1, y1, x2, y2 = map(int, xyxy)
-
-# вирізати об'єк з всього зображення
-# y -- відповідають за рядки
-# x -- відповідають за стовпчики
-roi = img[y1:y2, x1:x2]
-
-cv2.imshow('object', roi)
-
-
-# # відео
-# while True:
-#     success, img = cap.read()
+# import ultralytics
+# import cv2
 #
-#     if not success:
-#         break
+# # створення моделі
+# model = ultralytics.YOLO('yolov8s.pt')
 #
-#     img = cv2.resize(img, None, fx=0.5, fy=0.5)
+# # отримати зображення з ввідео
+# cap = cv2.VideoCapture('data/lesson8/cars+bikes.mp4')
+# success, img = cap.read()
 #
-#     results = model.predict(img)
-#     result = results[0]
+# img = cv2.resize(img, None, fx=0.5, fy=0.5)
 #
-#     res_img = result.plot()
+# cv2.imshow('original', img)
 #
-#     cv2.imshow('result', res_img)
+# # застосування моделі
+# # predict може отримувати декілька зображень за раз
+# # predict([img1, img2, img3]) і тому results це список
+# # [result1, result2, result3]
 #
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+# results = model.predict(
+#     img,    # саме зображення
+#     device='cpu',    # процесор де запускати модель
+#                      # cpu -- звичайний процес
+#                      # cuda -- графічний процесор
+#     conf=0.25,       # мінімальній відсоток для об'єкта щоб попасти в result
+#     iou=0.7      # максимально можливий рівень перетину рамок(якщо більше то вважаємо
+#                  # що це рамки для одного й того самого об'єкта)
+# )
+#
+#
+# # results -- список з одним елементом
+# result = results[0]
+#
+# # отримати назви класів(об'єкти які вмієме визначати модель)
+# names = result.names # dict --- id: str
+# print(names)
+#
+# # самі об'єкти
+# cls = result.boxes.cls
+# print(cls)
+#
+# # візуалізація результів
+# res_img = result.plot()
+# cv2.imshow('result', res_img)
+#
+#
+# # ймовірності
+# conf = result.boxes.conf
+# print(conf)
+#
+#
+# # рамка(box)
+# boxes = result.boxes
+#
+# # отримати перший об'єкт
+# box = boxes[0]
+#
+# print(box)
+#
+# # координати
+# xyxy = box.xyxy[0]
+#
+# # переведення координат в int
+# x1, y1, x2, y2 = map(int, xyxy)
+#
+# # вирізати об'єк з всього зображення
+# # y -- відповідають за рядки
+# # x -- відповідають за стовпчики
+# roi = img[y1:y2, x1:x2]
+#
+# cv2.imshow('object', roi)
+#
+#
+# # # відео
+# # while True:
+# #     success, img = cap.read()
+# #
+# #     if not success:
+# #         break
+# #
+# #     img = cv2.resize(img, None, fx=0.5, fy=0.5)
+# #
+# #     results = model.predict(img)
+# #     result = results[0]
+# #
+# #     res_img = result.plot()
+# #
+# #     cv2.imshow('result', res_img)
+# #
+# #     if cv2.waitKey(1) & 0xFF == ord('q'):
+# #         break
+#
+#
+# cv2.waitKey(0)
 
-
-cv2.waitKey(0)
